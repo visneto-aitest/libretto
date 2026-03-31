5310361b86838de183deee35ddb7b7aed482506f
Mochael <mkronovet@gmail.com>
2026-03-30 17:59:29 -0700

    increment version

--- packages/libretto/test/deploy-artifact.spec.ts
+++ packages/libretto/test/deploy-artifact.spec.ts
@@ -209,7 +209,7 @@ describe("createHostedDeployPackage", () => {
     const implementation = extractBundledImplementation(bundle);
 
     expect(deployManifest.dependencies).toEqual({
-      libretto: "0.5.3-experimental.4",
+      libretto: "0.5.3-experimental.6",
       lodash: "^4.17.21",
     });
     expect(bundle).toContain('export const testWorkflow = createWorkflowProxy("testWorkflow");');
